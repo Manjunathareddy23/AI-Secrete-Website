930bed73bb2c97818e1d5444cd2c5d37e1fd7d5f
Kummathi Manjunatha Reddy <141892277+Manjunathareddy23@users.noreply.github.com>
2025-06-01 00:28:40 +0530

    Create tts_gtts.py

---
+++ scripts/tts_gtts.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('text', type=str)
+parser.add_argument('--output_path', type=str, default="tmp.wav")
+args = parser.parse_args()
+
+tts = gTTS(args.text)
+tts.save(args.output_path)
